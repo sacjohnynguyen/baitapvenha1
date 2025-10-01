629a3b86558b5aeb068b39db0fcaee48d37899e8
sacjohnynguyen <quangsac3@gmail.com>
2025-10-01 13:51:40 +0700

    Add files via upload

---
+++ Information_student/DACN.java
@@ -0,0 +1,11 @@
+
+package Information_student;
+
+public class DACN {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFStudent jfs = new JFStudent();
+        jfs.show();
+    }
+}
